f3f7ef28f823a7077b04fa267f7cf1e34501620d
EC2 Default User <141287848+Shunta126@users.noreply.github.com>
2023-10-09 07:52:29 +0000

    add model_association

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,8 @@
 class Customer < ApplicationRecord
   has_one_attached :costomer_image
+  has_many :restaurants
+  has_many :likes
+  has_many :comments
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,7 @@
 class Restaurant < ApplicationRecord
   has_one_attached :food_image
+  belongs_to :genres
+  belongs_to :customers
+  has_many :likes
+  has_many :comments
 end
